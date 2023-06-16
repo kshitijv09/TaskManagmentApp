981dee7bf4aa50732a6f2b4b80530cd5d110bc1b
kshitijv09 <kshitijv09@gmail.com>
2023-06-16 23:12:58 +0530

    Prod changes2

--- app.js
+++ app.js
@@ -18,19 +18,9 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 const port = process.env.PORT || 5000;
 
-const rateLimiter = require("express-rate-limit");
-const xss = require("xss-clean");
-
 app.use(express.json());
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
-app.use(cors());
 
-app.use(xss());
+app.use(cors());
 
 app.use("/auth", authRouter);
 app.use("/user", authenticateUser, taskRouter);
